996e907af1902acbe725d9cd8d55aa7a6d587d35
Stanislav Krizhanovsky <slaviann@gmail.com>
2014-09-27 19:21:12 +0400

    js functions; basic chat alghoritm draft

--- js/chat.js
+++ js/chat.js
@@ -1,80 +1,134 @@
 // conf
 SERVER_URL = "http://127.0.0.1";
 SERVER_PORT = "5000";
+
+//global variables
+messages_count = 0
+
 URLS = {
-    "send": "/send",
-    "login": "/login",
-    "logout": "/logout"
+    "send": SERVER_URL + ":" + SERVER_PORT + "/messages/send",
+    "login": SERVER_URL + ":" + SERVER_PORT + "/login",
+    "logout": SERVER_URL + ":" + SERVER_PORT + "/logout",
+    "message": SERVER_URL + ":" + SERVER_PORT + "/messages/",
+    "messages_count": SERVER_URL + ":" + SERVER_PORT + "/messages/count",
+    "history": SERVER_URL + ":" + SERVER_PORT + "/history"
 }
 
-
-// functions
+// interface functions
 // send POST request
-// success and fails are callback functions
+// pass complete callback function
 function send_post(data, url, complete) {
-    console.log(url);
-    console.log(data);
     var future = $.ajax({
         type: "POST",
         url: url,
         data: data,
         contentType: 'application/json;charset=UTF-8'
     })
-    // future.success(success)
-    // future.error(error)
     future.complete(complete)
 }
 
-function clicks() {
-    // callback function
+// send GET request
+// complete: callback function
+function send_get(url, complete) {
+    var future = $.ajax({
+        type: "GET",
+        url: url,
+        contentType: 'application/json;charset=UTF-8'
+    })
+    future.complete(complete)
+}
 
+// main functions
+// load history to the chat window
+function load_history() {
+    complete = function (xhr, textStatus) {
+        var lines = "";
+        data = jQuery.parseJSON(xhr.responseText);
+        for (var i = 0; i < data.length; i++ ) {
+            lines = lines + data[i]['login'] + ": " + data[i]['message'] + "\n";
+        }
+        $("textarea#chat_area").val(lines);
+    }
+    send_get(URLS['history'], complete)
+}
+
+function load_messages_count() {
+    complete = function (xhr, textStatus) {
+        data = jQuery.parseJSON(xhr.responseText);
+        console.log(messages_count)
+        messages_count = data
+    }
+    send_get(URLS['messages_count'], complete)
+}
+
+function load_message() {
+    complete = function (xhr, textStatus) {
+        data = jQuery.parseJSON(xhr.responseText);
+        console.log(data)
+        //for (var i = 0; i < data.length; i++ ) {
+        //    lines = lines + data[i]['login'] + ": " + data[i]['message'] + "\n";
+        //}
+        $("textarea#chat_area").val(lines);
+    }
+}
+
+function login() {
+    data = $("input#login_input").val();
+    data = {
+        "login": data
+    }
+    complete = function (xhr, textStatus) {
+        if (xhr.status == 200) {
+            $("div#login").modal("hide");
+            load_messages_count();
+            load_message();
+        } else if (xhr.status == 403) {
+            alert("Данное имя уже присутсвует в чате. Выберите другое")
+        }
+    }
+    send_post(JSON.stringify(data), URLS['login'], complete)
+}
+
+function logout() {
+    data = $("input#login_input").val();
+    if(data){
+        console.log(data);
+        data = {
+            "login": data
+        };
+        send_post(JSON.stringify(data), URLS["logout"])
+    }
+}
+
+//all clicks here
+function clicks() {
     $("button#send_msg").click(function () {
         data = $("input#chat_input").val();
-        console.log(data)
+        $("input#chat_input").val();
+        var login = $("input#login_input").val();
         data = {
-            "message": data
+            "message": data,
+            "login": login
         }
-        url = SERVER_URL + ":" + SERVER_PORT + URLS['send']
-        send_post(JSON.stringify(data), url)
-        // alert( "Handler for .click() called." );
+        send_post(JSON.stringify(data), URLS['send'])
     });
 
     $("button#send_login").click(function () {
-        data = $("input#login_input").val();
-        console.log(data)
-        data = {
-            "login": data
-        }
-        // do login
-        url = SERVER_URL + ":" + SERVER_PORT + URLS['login']
-        complete = function (xhr, textStatus) {
-            if (xhr.status == 200) {
-                $("div#login").modal("hide")
-            } else if (xhr.status == 403) {
-                alert("Данное имя уже присутсвует в чате. Выберите другое")
-            }
-        }
-        send_post(JSON.stringify(data), url, complete)
+        login()
     });
 
     $("button#send_logout").click(function () {
-        data = $("input#login_input").val();
-        console.log(data)
-        data = {
-            "login": data
-        }
-        // do logout
-        url = SERVER_URL + ":" + SERVER_PORT + URLS['logout']
-        complete = function (xhr, textStatus) {
-            window.location.reload();
-        }
-        send_post(JSON.stringify(data), url, complete)
+        logout()
+        window.location.reload();
     });
 }
 
-
 // calls
 $(document).ready(function () {
+    // bind all clicks
     clicks()
+    // load all history
+    load_history()
+    $('textarea#chat_area').scrollTop($('textarea#chat_area')[0].scrollHeight);
     $("#login").modal("show")
 });
--- server.py
+++ server.py
@@ -1,26 +1,31 @@
 #!/usr/bin/env python
 
+import time
+import json
 from flask import Flask
 from flask import request, make_response
-import time
 
 app = Flask(__name__)
 
-messages = {}
+messages = []
 names = []
 options_headers = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-iOrigin': '*',
     'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
     'Access-Control-Max-Age': '100',
+    'Access-Control-Allow-Credentials': 'true',
     'Access-Control-Allow-Headers':
         'origin, x-csrftoken, content-type, accept',
     }
 
-@app.route("/send", methods=["POST", "OPTIONS"])
-def new():
+@app.route("/messages/send", methods=["POST", "OPTIONS"])
+def send():
     if request.get_json():
-        print request.get_json()
+        data = request.get_json()
+        print data
+        messages.append(data)
+        print messages
     if request.method == "OPTIONS":
         return make_response(("", 200, options_headers))
     return make_response(("", 200, options_headers))
@@ -33,7 +38,7 @@ def login():
         return make_response(("", 200, options_headers))
     name = data.get("login")
     if not name:
-        return make_response(("", 500))
+        return make_response(("", 403))
     if name in names:
         return make_response(("", 403, options_headers))
     else:
@@ -55,5 +60,33 @@ def logout():
     else:
         return make_response(("", 204, options_headers))
 
+@app.route("/messages/<int:num>", methods=["GET", "OPTIONS"])
+def get_messages():
+    if request.method == "OPTIONS":
+        return make_response(("", 200, options_headers))
+    else:
+        if messages:
+            data = messages[-1]
+        else:
+            data = ""
+        return make_response((json.dumps(data),  200, options_headers))
+
+@app.route("/messages/count", methods=["GET", "OPTIONS"])
+def get_messages_count():
+    if request.method == "OPTIONS":
+        return make_response(("", 200, options_headers))
+    else:
+        if messages:
+            data = messages[-1]
+        else:
+            data = ""
+        return make_response((json.dumps(len(messages)),  200, options_headers))
+
+@app.route("/history", methods=["GET", "OPTIONS"])
+def get_history():
+    if request.method == "OPTIONS":
+        return make_response(("", 200, options_headers))
+    return make_response((json.dumps(messages), 200, options_headers))
+
 if __name__ == "__main__":
-    app.run()
+    app.run(threaded=True)
